14b92eeb6b559e3909bac1f1cccfe551facdb08e
Lev111111 <93332030+LovieLevi@users.noreply.github.com>
2022-04-11 09:48:20 +0700

    a

---
+++ smuzi/index.js
@@ -0,0 +1,17 @@
+console.log('start indes.js')
+
+// var body = document.getElementById("body")
+// var line = document.getElementsByClassName("line")
+// var sayt = document.documentElement.scrollHeight
+// var vithsayt = window.screen.height
+
+// if (vithsayt > sayt) {
+// 	line[0].style.height = "100%"
+// } else {
+// 	console.log(vithsayt)
+// 	vithsayt *= 100
+// 	console.log(vithsayt)
+// 	vithsayt /= sayt
+// 	console.log(vithsayt)
+// 	line[0].style.height = vithsayt + window.scrollY +"%" 
+// }
